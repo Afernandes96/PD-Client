22832d290cc0a1b8cefc7505d7d0a82da221bbd3
André Fernandes <greenstuga@gmail.com>
2017-12-27 12:07:13 +0000

    2

--- PDClientTP/src/ui/gui/ThreeInRowView.java
+++ PDClientTP/src/ui/gui/ThreeInRowView.java
@@ -5,10 +5,13 @@ import three_in_row.logic.ObservableGame;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 public class ThreeInRowView extends JFrame implements Observer
@@ -18,25 +21,57 @@ public class ThreeInRowView extends JFrame implements Observer
     ThreeInRowGamePanel panel;
     Login login;
     
-    JMenuBar
+    JMenuBar menu;
+    JMenu jogo;
+    JMenu jogador;
+    JMenuItem historico;
+    JMenuItem disconnect;
+    JMenuItem exit;
+    
 
-    public ThreeInRowView(ObservableGame j)
+    public ThreeInRowView(MainM model)
     {
         super("Three in a row");
-
-        game = j;            
+        
+        this.model=model;
+         
         game.addObserver(this);
         panel=new ThreeInRowGamePanel(game);
 
         addComponents();
- 
+        initComponents();
+        listeners();
+        
         setVisible(true);
+                
         this.setSize(700, 500);
+        setLocation(130, 90);
         this.setMinimumSize(new Dimension(650, 450));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         validate();
+        model.getManager().addObserver(this);
     }
   
+    void initComponents(){
+        menu = new JMenuBar();
+        jogo = new JMenu("Jogo");
+        jogador= new JMenu("Perfil");
+        
+        historico=new JMenuItem("Histórico");
+        disconnect = new JMenuItem("Desconectar");
+        exit = new JMenuItem("Sair");
+        
+        jogo.add(disconnect);
+        jogo.add(exit);
+        
+        jogador.add(historico);
+        
+        menu.add(jogo);
+        menu.add(jogador);
+        
+        setJMenuBar(menu);
+   
+    }
     
     private void addComponents()
     {
@@ -45,6 +80,15 @@ public class ThreeInRowView extends JFrame implements Observer
         cp.setLayout(new BorderLayout());
         cp.add(panel,BorderLayout.CENTER);
     }
+    
+     protected void listeners() {
+         exit.addItemListener(new ActionListner(){
+             @Override
+             public void actionPerformed(ActionEvent e){
+                 System.exit(0);
+             }
+         }
+     }
 
     @Override
     public void update(Observable o, Object arg) {
